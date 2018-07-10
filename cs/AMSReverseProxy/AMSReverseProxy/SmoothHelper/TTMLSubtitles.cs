6558612a3a7f696ec9dcad8bce072cc9dd9505f6
Frédéric Le Coquil <flecoqui@microsoft.com>
2018-07-11 00:23:46 +0200

    Add TTML WebVTT converter

---
+++ cs/AMSReverseProxy/AMSReverseProxy/SmoothHelper/TTMLSubtitles.cs
@@ -0,0 +1,333 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMSReverseProxy.SmoothHelper
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Xml;
+
+    /// <summary>
+    /// Parses a Smooth Streaming Manifest.
+    /// </summary>
+     class TTMLSubtitles    {
+
+
+        public List<SubtitleItem> subtitleList;
+        public double TimeOffset =  0;
+        // tick = hundred nano second = 10^7
+        public const int TicksPerSecond = 10000000;
+
+        /// <summary>
+        /// Defines the TTMLSubtitles element.
+        /// </summary>
+        private const string TTMLSubtitlesElement = "tt";
+
+        /// <summary>
+        /// Defines the TTMLSubtitles head.
+        /// </summary>
+        private const string TTMLSubtitlesHead = "head";
+
+        /// <summary>
+        /// Defines the TTMLSubtitles body.
+        /// </summary>
+        private const string TTMLSubtitlesBody = "body";
+
+        /// <summary>
+        /// Defines the TTMLSubtitles div.
+        /// </summary>
+        private const string TTMLSubtitlesDiv = "div";
+
+        /// <summary>
+        /// Defines the TTMLSubtitles p.
+        /// </summary>
+        private const string TTMLSubtitlesP = "p";
+
+        /// <summary>
+        /// Defines the Manifest MajorVersion attribute.
+        /// </summary>
+        private const string ManifestMajorVersionAttribute = "MajorVersion";
+
+        /// <summary>
+        /// Defines the  begin attribute.
+        /// </summary>
+        private const string BeginAttribute = "begin";
+
+        /// <summary>
+        /// Defines the  end attribute.
+        /// </summary>
+        private const string EndAttribute = "end";
+
+        /// <summary>
+        /// Defines the  dur attribute.
+        /// </summary>
+        private const string DurationAttribute = "dur";
+
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SmoothStreamingManifestParser"/> class.
+        /// </summary>
+        /// <param name="subtitleStream">The stream of the manifest being parsed.</param>
+        public TTMLSubtitles()
+        {
+            TimeOffset = 0;
+            if (subtitleList == null)
+                subtitleList = new List<SubtitleItem>();
+        }
+        public TTMLSubtitles(double timeOffset)
+        {
+            TimeOffset = timeOffset;
+            if (subtitleList == null)
+                subtitleList = new List<SubtitleItem>();
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SmoothStreamingManifestParser"/> class.
+        /// </summary>
+        /// <param name="subtitleStream">The buffer of the manifest being parsed.</param>
+        public bool ParseTTMLSubtitles(byte[] manifestBuffer)
+        {
+            bool bResult = false;
+            using (var subtitleStream = new MemoryStream(manifestBuffer))
+            {
+                bResult = this.ParseTTMLSubtitles(subtitleStream);
+            }
+            return bResult;
+        }
+
+
+
+        /// <summary>
+        /// Adds attributes to the stream info.
+        /// </summary>
+        /// <param name="reader">The xml reader.</param>
+        /// <param name="streamInfo">The stream info.</param>
+        private static void AddAttributes(XmlReader reader, StreamInfo streamInfo)
+        {
+            if (reader.HasAttributes && reader.MoveToFirstAttribute())
+            {
+                do
+                {
+                    streamInfo.AddAttribute(reader.Name, reader.Value);
+                }
+                while (reader.MoveToNextAttribute());
+                reader.MoveToFirstAttribute();
+            }
+        }
+
+        /// <summary>
+        /// Adds attributes to the quality level.
+        /// </summary>
+        /// <param name="reader">The xml reader.</param>
+        /// <param name="qualityLevel">The quality level.</param>
+        private static void AddAttributes(XmlReader reader, QualityLevel qualityLevel)
+        {
+            if (reader.HasAttributes && reader.MoveToFirstAttribute())
+            {
+                do
+                {
+                    qualityLevel.AddAttribute(reader.Name, reader.Value);
+                }
+                while (reader.MoveToNextAttribute());
+                reader.MoveToElement();
+            }
+        }
+
+        /// <summary>
+        /// Adds custom attributes to the quality level.
+        /// </summary>
+        /// <param name="reader">The xml reader.</param>
+        /// <param name="qualityLevel">The quality level.</param>
+        private static void AddCustomAttributes(XmlReader reader, QualityLevel qualityLevel)
+        {
+            if (!reader.IsEmptyElement)
+            {
+                while (reader.Read())
+                {
+                    if (reader.Name == "CustomAttributes" && reader.NodeType == XmlNodeType.Element)
+                    {
+                        while (reader.Read())
+                        {
+                            if ((reader.Name == "Attribute") && (reader.NodeType == XmlNodeType.Element))
+                            {
+                                string attribute = reader.GetAttribute("Name");
+
+                                if (!string.IsNullOrEmpty(attribute))
+                                {
+                                    qualityLevel.AddCustomAttribute(attribute, reader.GetAttribute("Value"));
+                                }
+                            }
+
+                            if ((reader.Name == "CustomAttributes") && (reader.NodeType == XmlNodeType.EndElement))
+                            {
+                                return;
+                            }
+                        }
+                    }
+
+                    if (reader.Name == "QualityLevel" && reader.NodeType == XmlNodeType.EndElement)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+        string GetMultiLineText(XmlReader reader)
+        {
+            string Text = string.Empty;
+            while ((reader.Name == "") && (reader.NodeType == XmlNodeType.Whitespace))
+                reader.Read();
+            if (reader.NodeType == XmlNodeType.Element)
+            {
+                if (reader.Name == "span")
+                {
+
+                    reader.Read();
+                    while ((reader.Name == "") && (reader.NodeType == XmlNodeType.Whitespace))
+                        reader.Read();
+                    if (reader.NodeType == XmlNodeType.Text)
+                    {
+                        Text = reader.Value;
+                    }
+                    reader.Read();
+                    while ((reader.Name == "") && (reader.NodeType == XmlNodeType.Whitespace))
+                        reader.Read();
+                    while (((reader.Name == "br") || (reader.Name == "span")) && ((reader.NodeType == XmlNodeType.Element)|| (reader.NodeType == XmlNodeType.EndElement)))
+                    {
+                        reader.Read();
+                        while ((reader.Name == "") && (reader.NodeType == XmlNodeType.Whitespace))
+                            reader.Read();
+                        if (reader.NodeType == XmlNodeType.Text)
+                        {
+                            Text += "\n" + reader.Value;
+                        }
+                        reader.Read();
+                        while ((reader.Name == "") && (reader.NodeType == XmlNodeType.Whitespace))
+                            reader.Read();
+                    }
+                }
+            }
+            else if (reader.NodeType == XmlNodeType.Text)
+            {
+                Text = reader.Value;
+                reader.Read();
+                while ((reader.Name == "") && (reader.NodeType == XmlNodeType.Whitespace))
+                    reader.Read();
+                while ((reader.Name == "br") && (reader.NodeType == XmlNodeType.Element))
+                {
+                    reader.Read();
+                    while ((reader.Name == "") && (reader.NodeType == XmlNodeType.Whitespace))
+                        reader.Read();
+                    if (reader.NodeType == XmlNodeType.Text)
+                    {
+                        Text += "\n" + reader.Value;
+                    }
+                    reader.Read();
+                    while ((reader.Name == "") && (reader.NodeType == XmlNodeType.Whitespace))
+                        reader.Read();
+                }
+            }
+
+            return Text;
+        }
+        /// <summary>
+        /// Parses the manifest stream.
+        /// </summary>
+        /// <param name="subtitleStream">The manifest stream being parsed.</param>
+        public bool ParseTTMLSubtitles(Stream subtitleStream)
+        {
+            bool bResult = false;
+            try
+            {
+                using (XmlReader reader = XmlReader.Create(subtitleStream))
+                {
+                    if (reader.Read() && reader.IsStartElement(TTMLSubtitlesElement))
+                    {
+                        while (reader.Read())
+                        {
+                            if (reader.Name == TTMLSubtitlesBody && reader.NodeType == XmlNodeType.Element)
+                            {
+                                reader.Read();
+                                while ((reader.Name == "") && (reader.NodeType == XmlNodeType.Whitespace))
+                                    reader.Read();
+                                if (reader.Name == TTMLSubtitlesDiv && reader.NodeType == XmlNodeType.Element)
+                                {
+
+                                    while (((reader.Name == TTMLSubtitlesP) && (reader.NodeType == XmlNodeType.Element)) || reader.Read())
+                                    {
+                                        while ((reader.Name == "") && (reader.NodeType == XmlNodeType.Whitespace))
+                                            reader.Read();
+                                        if (reader.Name == TTMLSubtitlesP && reader.NodeType == XmlNodeType.Element)
+                                        {
+                                            string Text = string.Empty;
+                                            string Id = string.Empty;
+                                            string Begin = string.Empty;
+                                            string End = string.Empty;
+                                            string Dur = string.Empty;
+                                            if (reader.HasAttributes)
+                                            {
+                                                for (int attInd = 0; attInd < reader.AttributeCount; attInd++)
+                                                {
+                                                    reader.MoveToAttribute(attInd);
+                                                    if (reader.NodeType == XmlNodeType.Attribute)
+                                                    {
+                                                        if (reader.Name == "begin")
+                                                            Begin = reader.Value;
+                                                        else if (reader.Name == "dur")
+                                                            Dur = reader.Value;
+                                                        else if (reader.Name == "end")
+                                                            End = reader.Value;
+                                                        else if (reader.Name == "xml:id")
+                                                            Id = reader.Value;
+                                                    }
+                                                }
+                                            }
+                                            reader.Read();
+
+                                            Text = GetMultiLineText(reader);
+
+                                            if (!string.IsNullOrEmpty(Text) &&
+                                                !string.IsNullOrEmpty(Begin) &&
+                                                !string.IsNullOrEmpty(End)
+                                                )
+                                            {
+
+                                                System.Diagnostics.Debug.WriteLine("Subtitle: " + Id + " Content: \r\n" + Text);
+                                                ulong BeginTime = SubtitleItem.ParseTime(Begin);
+                                                ulong EndTime = SubtitleItem.ParseTime(End);
+
+                                                SubtitleItem item = new SubtitleItem((ulong)TimeOffset + BeginTime, (ulong)TimeOffset + EndTime, Text);
+                                                if (item != null)
+                                                    subtitleList.Add(item);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                bResult = true;
+            }
+            catch(Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception while parsing Subtitles file: " + ex.Message );
+                bResult = false;
+            }
+            return bResult;
+        }
+    }
+}
--- cs/AMSReverseProxy/AMSReverseProxy/Startup.cs
+++ cs/AMSReverseProxy/AMSReverseProxy/Startup.cs
@@ -89,6 +89,32 @@ namespace AMSReverseProxy
             }
             return false;
         }
+        public string ParseTTMLChunk(byte[] data)
+        {
+            string result = string.Empty;
+            if ((data != null) && (data.Length > 0))
+            {
+                int index = 0;
+                while (index < data.Length)
+                {
+                    AMSReverseProxy.SmoothHelper.Mp4Box box = AMSReverseProxy.SmoothHelper.Mp4Box.CreateMp4Box(data, index);
+                    if (box.GetBoxType() == "moof")
+                    {
+                        index += box.GetBoxLength();
+                    }
+                    else if (box.GetBoxType() == "mdat")
+                    {
+                        result = System.Text.Encoding.UTF8.GetString(box.GetBoxData(), 0, box.GetBoxData().Length);
+                        index += box.GetBoxLength();
+                    }
+                    else 
+                    {
+                        index += box.GetBoxLength();
+                    }
+                }
+            }
+            return result;
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -104,28 +130,113 @@ namespace AMSReverseProxy
                     await context.Response.WriteAsync("Hello World!");
                 else
                 {
-                    string Host = "http://amssamples.streaming.mediaservices.windows.net/";
+                    string Host = "http://localhost/";
+                 //   string Host = "http://srgtest-accountdigitalinnovation-euwe.channel.media.azure.net/";
+                    //string Host = "http://amssamples.streaming.mediaservices.windows.net/";
                     System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
                     var response = await client.GetAsync(new Uri(Host + Path));
                     if (response != null)
                     {
+
                         context.Response.ContentType = response.Content.Headers.ContentType.MediaType;
                         context.Response.ContentLength = response.Content.Headers.ContentLength.Value;
                         if(IsSmoothStreamingManifest(Host + Path, context.Response.ContentType))
                         {
                             System.Diagnostics.Debug.Write("SmoothStreaming Manifest: " + Host + Path + " ContentType: " + context.Response.ContentType);
+
+                            SmoothHelper.ManifestManager mm = SmoothHelper.ManifestManager.CreateManifestCache(new Uri(Host + Path), false, 5000000, 20);
+                            if(mm != null)
+                            {
+                                if ((response.Headers.Location != null) && (response.Headers.Location != new Uri(Host + Path)))
+                                {
+                                    mm.RedirectUri = response.Headers.Location;
+                                    mm.RedirectBaseUrl = SmoothHelper.ManifestManager.GetBaseUri(mm.RedirectUri.AbsoluteUri);
+                                }
+                                else
+                                {
+                                    mm.RedirectBaseUrl = string.Empty;
+                                    mm.RedirectUri = null;
+                                }
+                                mm.BaseUrl = SmoothHelper.ManifestManager.GetBaseUri(new Uri(Host + Path).AbsoluteUri);
+                                bool result = mm.ParseManifest(await response.Content.ReadAsByteArrayAsync());
+                                if(result == true)
+                                {
+                                    System.Diagnostics.Debug.Write("SmoothStreaming Manifest: " + Host + Path + " correctly downloaded and parsed ");
+                                    
+                                    // Download Text Chunks
+                                    if ((mm.TextChunkListList != null) && (mm.TextChunkListList.Count > 0))
+                                    {
+                                        // Something to download
+                                        if (!mm.IsDownloadCompleted(mm.TextChunkListList))
+                                        {
+                                            System.Diagnostics.Debug.WriteLine(string.Format("{0:d/M/yyyy HH:mm:ss.fff}", DateTime.Now) + " downloadThread downloading text chunks for Manifest Uri: " + mm.ManifestUri.ToString());
+                                            foreach (var cl in mm.TextChunkListList)
+                                            {
+                                                SmoothHelper.ChunkBuffer cb;
+                                                while (cl.ChunksToReadQueue.TryDequeue(out cb))
+                                                {
+                                                    string url = (string.IsNullOrEmpty(mm.RedirectBaseUrl) ? mm.BaseUrl : mm.RedirectBaseUrl) + "/" + cl.TemplateUrl.Replace("{start_time}", cb.Time.ToString());
+                                                    System.Diagnostics.Debug.WriteLine(string.Format("{0:d/M/yyyy HH:mm:ss.fff}", DateTime.Now) + " downloadThread downloading text chunks : " + url.ToString());
+                                                    cb.chunkBuffer = await mm.DownloadChunkAsync(new Uri(url));
+
+                                                    if (cb.IsChunkDownloaded())
+                                                    {
+                                                        ulong l = cb.GetLength();
+                                                        string text = ParseTTMLChunk(cb.chunkBuffer);
+                                                        double time = mm.TimescaleToHNS(cb.Time) / (SmoothHelper.ManifestManager.TimeUnit);
+                                                        
+                                                        System.Diagnostics.Debug.Write("TTML file at : " + time.ToString() + " seconds \r\n" + text );
+                                                        System.Text.Encoding.UTF8.GetBytes(text);
+                                                        SmoothHelper.TTMLSubtitles tTMLSubtitles = new SmoothHelper.TTMLSubtitles(time);
+                                                        if(tTMLSubtitles!=null)
+                                                        {
+                                                            if(tTMLSubtitles.ParseTTMLSubtitles(System.Text.Encoding.UTF8.GetBytes(text))==true)
+                                                            {
+                                                                if (tTMLSubtitles.subtitleList != null)
+                                                                {
+                                                                    long count = tTMLSubtitles.subtitleList.LongCount();
+                                                                    System.Diagnostics.Debug.WriteLine(string.Format("{0:d/M/yyyy HH:mm:ss.fff}", DateTime.Now) + count.ToString() + " subtitles found in the chunk");
+                                                                }
+                                                            }
+                                                        }
+                                                        cl.InputBytes += l;
+                                                        cl.InputChunks++;
+
+                                                        cl.ChunksQueue.Enqueue(cb);
+                                                    }
+                                                }
+                                            }                                       
+                                            System.Diagnostics.Debug.WriteLine(string.Format("{0:d/M/yyyy HH:mm:ss.fff}", DateTime.Now) + " downloadThread downloading text chunks done for Uri: " + mm.ManifestUri.ToString());
+                                        }
+                                        else
+                                        {
+                                            if (mm.IsLive)
+                                                result = true;
+                                        }
+                                    }
+                                }
+                                context.Response.Redirect(Host + Path);
+                                await response.Content.CopyToAsync(context.Response.Body);
+                                //var buffer = mm.UpdateSmoothManifestWithAbsoluteUri(await response.Content.ReadAsByteArrayAsync());
+
+                                //using (var manifestStream = new System.IO.MemoryStream(buffer))
+                                //{
+                                //    context.Response.ContentLength = buffer.LongLength;
+                                //    await manifestStream.CopyToAsync(context.Response.Body);
+                                //}
+                            }
                         }
                         else if (IsHLSManifest(Host + Path, context.Response.ContentType))
                         {
                             System.Diagnostics.Debug.Write("HLS Manifest: " + Host + Path + " ContentType: " + context.Response.ContentType);
-
+                            await response.Content.CopyToAsync(context.Response.Body);
                         }
                         else if (IsDASHManifest(Host + Path, context.Response.ContentType))
                         {
                             System.Diagnostics.Debug.Write("DASH Manifest: " + Host + Path + " ContentType: " + context.Response.ContentType);
-
+                            await response.Content.CopyToAsync(context.Response.Body);
                         }
-                        await response.Content.CopyToAsync(context.Response.Body);
+
                     }
 
                 }
